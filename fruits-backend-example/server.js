0d84fb469657473fb723e2753965b1c376b48285
Weiwei <linweiwei2222@163.com>
2023-12-11 14:25:10 -0500

    practice

--- fruits-backend-example/server.js
+++ fruits-backend-example/server.js
@@ -1,12 +1,22 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-const fruits = require("./models/fruits.js");
-const vegetables = require("./models/vegetables.js");
+const Fruit = require("./models/fruit");
+const Vegetable = require("./models/vegetable");
 const jsxViewEngine = require("jsx-view-engine");
 
 app.set("view engine", "jsx");
 app.set("views", "./views");
 app.engine("jsx", jsxViewEngine());
+
+const mongoURI = process.env.MONGO_URI;
+const db = mongoose.connetction;
+
+mongoose.connect(mongoURI);
+mongoose.connection.once("open", () => {
+  console.log("connected to mongo");
+});
 
 // ================ Middleware ================
 //
@@ -24,53 +34,86 @@ app.get("/", (req, res) => {
   );
 });
 
-app.get("/fruits/", (req, res) => {
-  res.render("fruits/Index", { fruits: fruits });
+app.get("/fruits/", async (req, res) => {
+  try {
+    const foundFruits = await Fruit.find({});
+    res.status(200).render("fruits/Index", { fruits: foundFruits });
+  } catch (err) {
+    res.status(400).send(err);
+  }
+  // res.render("fruits/Index", { fruits: fruits });
 });
 
 app.get("/fruits/new", (req, res) => {
   res.render("fruits/New");
 });
 
-app.post("/fruits", (req, res) => {
+app.post("/fruits", async (req, res) => {
   if (req.body.readyToEat === "on") {
+    //if checked, req.body.readyToEat is set to 'on'
     req.body.readyToEat = true;
   } else {
+    //if not checked, req.body.readyToEat is undefined
     req.body.readyToEat = false;
   }
-  fruits.push(req.body);
-  res.redirect("/fruits");
+
+  try {
+    const createdFruit = await Fruit.create(req.body);
+    res.status(200).redirect("/fruits");
+  } catch (err) {
+    res.status(400).send(err);
+  }
 });
 
-app.get("/fruits/:indexOfFruitsArray", (req, res) => {
-  res.render("fruits/Show", {
-    fruit: fruits[req.params.indexOfFruitsArray],
-  });
+app.get("/fruits/:id", async (req, res) => {
+  try {
+    const foundFruit = await Fruit.findById(req.params.id);
+    res.render("fruits/Show", {
+      fruit: foundFruit,
+    });
+  } catch (err) {
+    res.status(400).send(err);
+  }
 });
 
 // --------------vegetables -----------
-app.get("/vegetables/", (req, res) => {
-  res.render("vegetables/Index", { vegetables: vegetables });
+app.get("/vegetables/", async (req, res) => {
+  try {
+    const foundVegetables = await Vegetable.find({});
+    res.render("vegetables/Index", { vegetables: foundVegetables });
+  } catch (err) {
+    res.status(400).send(err);
+  }
 });
 
 app.get("/vegetables/new", (req, res) => {
   res.render("vegetables/New");
 });
 
-app.post("/vegetables", (req, res) => {
+app.post("/vegetables", async (req, res) => {
   if (req.body.readyToEat === "on") {
     req.body.readyToEat = true;
   } else {
     req.body.readyToEat = false;
   }
-  vegetables.push(req.body);
-  res.redirect("/vegetables");
+
+  try {
+    const createdVegetable = await Vegetable.create(req.body);
+    res.status(200).redirect("/vegetables");
+  } catch (err) {
+    res.status(400).send(err);
+  }
 });
 
-app.get("/vegetables/:indexOfVegetablesArray", (req, res) => {
-  res.render("vegetables/Show", {
-    vegetable: vegetables[req.params.indexOfVegetablesArray],
-  });
+app.get("/vegetables/:id", async (req, res) => {
+  try {
+    const foundVegetable = await Vegetable.findById(req.params.id);
+    res.render("vegetables/Show", {
+      vegetable: foundVegetable,
+    });
+  } catch (err) {
+    res.status(400).send(err);
+  }
 });
 
 // -------------------------
